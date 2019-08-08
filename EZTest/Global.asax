4052f216e164400bbe17afc0fdc493c8ef7b9345
GrimTheFallen <45598885+GrimTheFallen@users.noreply.github.com>
2019-08-08 12:04:14 +0200

    Add project files.

---
+++ EZTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EZTest.MvcApplication" Language="C#" %>
---
+++ EZTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EZTest.Startup))]
+namespace EZTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
